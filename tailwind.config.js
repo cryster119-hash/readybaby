997ae4e7681100d49261702fa014d226e6955aa5
KH PARK <cryster119@gmail.com>
2026-05-06 22:19:11 +0900

    UI 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 src 폴더 안의 모든 코드에 디자인을 적용하라는 뜻
   ],
   theme: {
     extend: {},
